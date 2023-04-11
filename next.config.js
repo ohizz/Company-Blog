2a97b715bb66f42c418b8702c0d03df4ee4f397f
ohizz <jtsamuel3084@gmail.com>
2023-04-11 14:49:14 +0100

    nextjscofig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 }
 
+module.exports = nextConfig
